138980fbb183ea78b68146c71543bb8357ccf83e
Harshit Jain <harshitostwal1234@gmail.com>
2025-08-21 15:27:47 +0530

    Add logo assets and image component
    - Created a new Logos index file to manage logo imports and exports.
    - Added logo assets for transparent black and white versions.
    - Updated the main assets index to include the new Logos.
    - Implemented a reusable ImageComp component for optimized image handling with Next.js.

--- src/app/(routes)/auth/register/page.jsx
+++ src/app/(routes)/auth/register/page.jsx
@@ -24,6 +24,8 @@ import {
 import { Gender } from "@/generated/prisma";
 import { useRegister } from "@/hooks/useUser";
 import { toast } from "sonner";
+import { ImageComp } from "@/components/ui/image";
+import { Assets } from "@/assets";
 
 function Page() {
   const registerForm = useForm({
@@ -55,118 +57,133 @@ function Page() {
   };
 
   return (
-    <Container className={"max-w-2xl flex flex-col justify-center gap-6"}>
-      <div className="flex flex-col gap-2">
-        <Heading size="h4">Registration</Heading>
-        <Heading size="p" className={"text-muted-foreground"}>
-          Please fill in the details below to create an account.
-        </Heading>
-      </div>
-      <Form {...registerForm}>
-        <form
-          className="flex flex-col gap-4"
-          onSubmit={registerForm.handleSubmit(onSubmit)}
-        >
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-start">
-            <FormField
-              name="firstName"
-              control={registerForm.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input placeholder={"First Name"} {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              name="lastName"
-              control={registerForm.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input placeholder={"Last Name"} {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              name="gender"
-              control={registerForm.control}
-              render={({ field }) => (
-                <FormItem>
-                  <Select value={field.value} onValueChange={field.onChange}>
-                    <FormControl>
-                      <SelectTrigger className={"w-full"}>
-                        <SelectValue placeholder="Select Gender">
-                          {field.value}
-                        </SelectValue>
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      <SelectItem value={Gender.MALE}>{Gender.MALE}</SelectItem>
-                      <SelectItem value={Gender.FEMALE}>
-                        {Gender.FEMALE}
-                      </SelectItem>
-                      <SelectItem value={Gender.OTHER}>
-                        {Gender.OTHER}
-                      </SelectItem>
-                    </SelectContent>
-                  </Select>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              name="age"
-              control={registerForm.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input placeholder={"Age"} type={"tel"} {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              name="mobileNo"
-              control={registerForm.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input placeholder={"Mobile No"} type={"tel"} {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              name="email"
-              control={registerForm.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input placeholder={"Email"} type={"email"} {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-
-          <Button
-            type="submit"
-            className={"w-full"}
-            isLoading={isPending}
-            disabled={isPending}
+    <Container className={"max-w-2xl flex flex-col justify-center gap-3"}>
+      <ImageComp
+        src={Assets.logos.transparent.black.src}
+        alt={Assets.logos.alt}
+        width={200}
+      />
+      <div className="flex flex-col gap-6 p-3">
+        <div>
+          <Heading size="h4" className={"font-bold"}>
+            Registration
+          </Heading>
+          <Heading size="p" className={"text-muted-foreground"}>
+            Please fill in the details below to create an account.
+          </Heading>
+        </div>
+        <Form {...registerForm}>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={registerForm.handleSubmit(onSubmit)}
           >
-            Register
-          </Button>
-        </form>
-      </Form>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-start">
+              <FormField
+                name="firstName"
+                control={registerForm.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder={"First Name"} {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name="lastName"
+                control={registerForm.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder={"Last Name"} {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name="gender"
+                control={registerForm.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <Select value={field.value} onValueChange={field.onChange}>
+                      <FormControl>
+                        <SelectTrigger className={"w-full"}>
+                          <SelectValue placeholder="Select Gender">
+                            {field.value}
+                          </SelectValue>
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value={Gender.MALE}>
+                          {Gender.MALE}
+                        </SelectItem>
+                        <SelectItem value={Gender.FEMALE}>
+                          {Gender.FEMALE}
+                        </SelectItem>
+                        <SelectItem value={Gender.OTHER}>
+                          {Gender.OTHER}
+                        </SelectItem>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name="age"
+                control={registerForm.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder={"Age"} type={"tel"} {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name="mobileNo"
+                control={registerForm.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        placeholder={"Mobile No"}
+                        type={"tel"}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name="email"
+                control={registerForm.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder={"Email"} type={"email"} {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <Button
+              type="submit"
+              className={"w-full"}
+              isLoading={isPending}
+              disabled={isPending}
+            >
+              Register
+            </Button>
+          </form>
+        </Form>
+      </div>
     </Container>
   );
 }
---
+++ src/assets/Logos/index.js
@@ -0,0 +1,15 @@
+import BlackTransparent from "@/assets/Logos/Transparent/Black.svg";
+import WhiteTransparent from "@/assets/Logos/Transparent/White.svg";
+
+export const Logos = {
+  transparent: {
+    white: {
+      src: WhiteTransparent,
+    },
+    black: {
+      src: BlackTransparent,
+    },
+  },
+
+  alt: "Nayi Disha",
+};
